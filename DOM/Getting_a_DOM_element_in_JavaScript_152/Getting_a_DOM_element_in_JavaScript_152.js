6d1e460bb8926b7ebfa4a73ed5826166b938b223
Elena Abramova <ea8366@gmail.com>
2022-01-14 15:34:45 +0300

    Getting a DOM element in JavaScript

---
+++ DOM/Getting_a_DOM_element_in_JavaScript_152/Getting_a_DOM_element_in_JavaScript_152.js
@@ -0,0 +1,35 @@
+/* Задача 1
+Даны 3 абзаца: Запишите ссылку на каждый из абзацев в отдельную переменную и выведите содержимое каждой из этих переменных в консоль. */
+
+let elem1 = document.querySelector('#elem1');
+console.log(elem1);
+
+let elem2 = document.querySelector('#elem2');
+console.log(elem2);
+
+let elem3 = document.querySelector('#elem3');
+console.log(elem3)
+
+
+
+/* Задача 2
+Дан следующий HTML: Получите ссылку на первый абзац из дива с id, равным block. */
+
+let block = document.querySelector('#block h3');
+console.log(block)
+
+
+
+/* Задача 3
+Дан следующий HTML: Получите ссылку на первый абзац из дива с классом block. */
+
+let block1 = document.querySelector('.block1 h4');
+console.log(block1)
+
+
+
+/* Задача 4
+Дан следующий HTML: Получите ссылку на первый абзац с классом www. */
+
+let num = document.querySelector('.www');
+console.log(num) 
